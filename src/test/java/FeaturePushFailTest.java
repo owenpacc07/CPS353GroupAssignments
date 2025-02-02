3d291b03e158ba90cd1bf1ede635d8a2e2ea2f68
Kevin <kevinculkin210@gmail.com>
2025-02-02 16:00:15 -0500

    Reran gradle wrapper command

---
+++ src/test/java/FeaturePushFailTest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class FeaturePushFailTest {
+
+	public static void main(String[] args) {
+		
+		if (true)
+			System.out.println("Fail Checkstyle");
+		if true
+			System.out.println("Fail Gradle");
+		if (true) {
+			
+		}
+
+	}
+
+}
