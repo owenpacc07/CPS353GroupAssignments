e6a5a20be9df25f2777eeb0d73714fdda660dcfa
Kevin <kevinculkin210@gmail.com>
2025-02-02 16:09:47 -0500

    Fixed FeaturePushFailTest.java to correctly compile and hopefully pass checkstyle.

--- src/test/java/FeaturePushFailTest.java
+++ src/test/java/FeaturePushFailTest.java
@@ -1,16 +1,9 @@
-import java.util.*;
 public class FeaturePushFailTest {
 
 	public static void main(String[] args) {
-		
-		if (true)
-			System.out.println("Fail Checkstyle");
-		if true
-			System.out.println("Fail Gradle");
 		if (true) {
-			
+			System.out.println("Should pass both actions now");
 		}
-
 	}
 
 }
