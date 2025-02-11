1f8eac3542d803554abf1864bcca5d752467be3b
owenpacc07 <owenpacc@gmail.com>
2025-02-11 13:53:12 -0500

    Added annotation files into respective packages in src and test

--- src/projectAnnotations/ConceptualAPI.java
+++ src/projectAnnotations/ConceptualAPI.java
@@ -1,4 +1,4 @@
-package project.annotations;
+package projectAnnotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- test/testProjectAnnotations/Utils.java
+++ test/testProjectAnnotations/Utils.java
@@ -1,4 +1,4 @@
-package project.annotations;
+package testProjectAnnotations;
 
 import java.io.File;
 import java.util.ArrayList;
