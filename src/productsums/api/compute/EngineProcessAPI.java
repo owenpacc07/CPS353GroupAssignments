e0bacac6a3653915442c8c6ed849f4b3a3409db7
Kevin <kevinculkin210@gmail.com>
2025-02-13 19:32:08 -0500

    Modified EngineAPI to use its own records for input and output instead of reusing the DataStorage Request/Response classes.

--- src/productsums/api/compute/EngineProcessAPI.java
+++ src/productsums/api/compute/EngineProcessAPI.java
@@ -1,12 +1,12 @@
 package productsums.api.compute;
 
-import productsums.models.process.DataStorageProcessRequest;
-import productsums.models.process.DataStorageProcessResponse;
+import productsums.models.compute.EngineInput;
+import productsums.models.compute.EngineOutput;
 import projectannotations.ConceptualAPI;
 import projectannotations.ConceptualAPIPrototype;
 
 @ConceptualAPI
 public interface EngineProcessAPI {
 	@ConceptualAPIPrototype
-	public DataStorageProcessResponse compute(DataStorageProcessRequest request);
+	public EngineOutput compute(EngineInput request);
 }
