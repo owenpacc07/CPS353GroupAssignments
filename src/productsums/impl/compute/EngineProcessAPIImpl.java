4ff802c999f2b035e7aab28ae4ccace72cf405ae
KevinTheC <132614981+KevinTheC@users.noreply.github.com>
2025-03-06 21:54:10 -0500

    Added Constants class. (#66)
    * Added Constants class.
    
    See issue #60. Did not survey the entire project for constants.
    
    * Changed test for large range of K to use constants.
    
    * Created Sentinel values for thrown exceptions.
    
    Created a sentinel value to represent null pointer exception, arithmetic exception, and illegal argument exception. This satisfies #57 and #52.
    
    * Integration test for Compute Engine exceptions
    
    See #59, added a short integration test to make sure sentinels are properly being sent out.
    
    * Checkstyle fix

--- src/productsums/impl/compute/EngineProcessAPIImpl.java
+++ src/productsums/impl/compute/EngineProcessAPIImpl.java
@@ -3,24 +3,30 @@ package productsums.impl.compute;
 import productsums.api.compute.EngineProcessAPI;
 import productsums.models.compute.EngineInput;
 import productsums.models.compute.EngineOutput;
+import productsums.utils.Constants;
 
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class EngineProcessAPIImpl implements EngineProcessAPI {
-	private final int maxSearch = 1000000;
-	private final List<Integer> primes = 
-			List.of(1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47);
-	
-	
     public EngineProcessAPIImpl() {
     	
     }
 
     @Override
     public EngineOutput compute(EngineInput request) {
-    	Result temp = calculateKNum(request.inputIndex());
+    	if (request == null) {
+    		return EngineOutput.nullPointer();
+    	}
+    	Result temp = null; 
+    	try {
+    		temp = calculateKNum(request.inputIndex());
+    	} catch (ArithmeticException e) {
+    		return EngineOutput.incomputableK();
+    	} catch (IllegalArgumentException e) {
+    		return EngineOutput.outOfBounds();
+    	}
     	List<Integer> ones = new LinkedList<>();
     	for (int i = 0; i < (temp.k - temp.factors.size()); i++) {
     		ones.add(1);
@@ -34,7 +40,7 @@ public class EngineProcessAPIImpl implements EngineProcessAPI {
      * @param num
      * @return Contains a List of all combinations of two factors that multiply to num.
      */
-	public LinkedList<LinkedList<Integer>> factors(int num) {
+	private LinkedList<LinkedList<Integer>> factors(int num) {
 		LinkedList<LinkedList<Integer>> listOfFactors = new LinkedList<>();
 		int upperlimit = ((int) Math.sqrt(num))+1;
 		for (int i = 2; i < upperlimit; i++) {
@@ -52,7 +58,7 @@ public class EngineProcessAPIImpl implements EngineProcessAPI {
 	 * @param List of all sets of factors.
 	 * @return Whether or not the largest group of factors was split.
 	 */
-	public boolean splitLargest(LinkedList<LinkedList<Integer>> list) {
+	private boolean splitLargest(LinkedList<LinkedList<Integer>> list) {
 		LinkedList<Integer> largest = list.get(0);
 		list.remove(0);
 		if (isPrime(largest.getLast())) {
@@ -81,12 +87,15 @@ public class EngineProcessAPIImpl implements EngineProcessAPI {
 	 * The kth product-sum number must be under EngineProcessAPIImpl.MAX_SEARCH.
 	 * @throws IllegalArgumentException k must not be less than one. The algorithm is designed for natural numbers.
 	 */
-	public Result calculateKNum(int k) throws ArithmeticException, IllegalArgumentException{
-		if (k < 1) {
+	private Result calculateKNum(int k) throws ArithmeticException, IllegalArgumentException{
+		if (k < Constants.MINIMUM_K) {
+			throw new IllegalArgumentException("Product sum cannot be calculated for non-natural numbers.");
+		}
+		if (k > Constants.MAXIMUM_K) {
 			throw new IllegalArgumentException("Product sum cannot be calculated for non-natural numbers.");
 		}
 		int curr = 4;
-		while (curr < maxSearch) {
+		while (curr > -1) {
 			LinkedList<LinkedList<Integer>> factorlist = factors(curr);
 			boolean factorsCanBeSplit = true;
 			while (factorsCanBeSplit && factorlist.size() > 0) {
@@ -97,28 +106,27 @@ public class EngineProcessAPIImpl implements EngineProcessAPI {
 			}
 			curr++;
 		}
-		throw new ArithmeticException("Bounds for function calculation exceeded. No number under " + maxSearch + " is a product-sum for number k " + k);
+		throw new ArithmeticException("Product-sum number for " + k + " cannot be represented by an integer. Integer overflow has occured where should not be possible.");
 	}
-    
 	/*
 	 * 						Single line functions
 	 */
     
-	public boolean isPrime(int i) {
-		return primes.stream()
+	private boolean isPrime(int i) {
+		return Constants.PRIMES.stream()
 				.anyMatch((item)->item == i);
 	}
-	public boolean allPrime(LinkedList<Integer> l) {
+	private boolean allPrime(LinkedList<Integer> l) {
 		return l.stream().allMatch((item)->isPrime(item));
 	}
-	public int sum(LinkedList<Integer> addends) {
+	private int sum(LinkedList<Integer> addends) {
 		return addends.stream().mapToInt(Integer::intValue).sum();
 	}
-	public void sort(LinkedList<LinkedList<Integer>> list) {
+	private void sort(LinkedList<LinkedList<Integer>> list) {
 		list.sort((l1,l2)->Integer.compare(sum(l2), sum(l1)));
 	}
-	public boolean validateK(LinkedList<Integer> factors, int k, int num) {
+	private boolean validateK(LinkedList<Integer> factors, int k, int num) {
 		return k - factors.size() == num - sum(factors);
 	}
-	public record Result(LinkedList<Integer> factors, int k, int curr) {}
+	private record Result(LinkedList<Integer> factors, int k, int curr) {}
 }
