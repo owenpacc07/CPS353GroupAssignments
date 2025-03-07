292d884052a407859263c04cccd75eb069194533
KevinTheC <132614981+KevinTheC@users.noreply.github.com>
2025-03-06 19:38:56 -0500

    Integration test for Compute Engine exceptions
    See #59, added a short integration test to make sure sentinels are properly being sent out.

--- test/productsums/integrationtest/impl/ExceptionGuardingTest.java
+++ test/productsums/integrationtest/impl/ExceptionGuardingTest.java
@@ -1,11 +1,31 @@
 package productsums.integrationtest.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import productsums.api.compute.EngineProcessAPI;
+import productsums.impl.compute.EngineProcessAPIImpl;
+import productsums.models.compute.EngineInput;
+import productsums.models.compute.EngineOutput;
 
 
 public class ExceptionGuardingTest {
 	@Test
 	public void testEngineImpl() {
-		
+		EngineProcessAPI api = new EngineProcessAPIImpl();
+		try {
+			Assertions.assertTrue(api.compute(null) == EngineOutput.nullPointer(), "Null pointer sentinel value was not returned.");
+			Assertions.assertTrue(api.compute(new EngineInput(1)) == EngineOutput.outOfBounds(), "K value out of bounds sentinel value was not returned.");
+		} catch (Exception e) {
+			Assertions.assertTrue(false, "Uncaught Exception thrown into process boundary: " + e);
+		}
+	}
+	@Test
+	public void testDataStorageImpl() {
+		//TODO can be implemented if necessary
+	}
+	@Test
+	public void testCoordinatorImpl() {
+		//TODO can be implemented if necessary
 	}
 }
