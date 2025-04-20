32584c196596978dc1b5bcd512436467c78c6dc7
KevinTheC <132614981+KevinTheC@users.noreply.github.com>
2025-04-20 12:47:50 -0400

    Pull request for a basic end to end test and grpc client (#86)
    * Added E2E test and client.
    
    Added basic end to end test, only uses console to console option on client side currently. Client was also added with CLI I/O
    
    * Added E2E test and client.
    
    Added basic end to end test, only uses console to console option on client side currently. Client was also added with CLI I/O
    
    * Modified E2E test and fixed checkstyle.
    
    Changed E2E test to run without requiring file creation permissions

---
+++ test/productsums/coordinator/EndToEndTest.java
@@ -0,0 +1,48 @@
+package productsums.coordinator;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import network.grpc.GrpcClient;
+import network.grpc.GrpcServer;
+
+public class EndToEndTest {
+	@Test
+	public void testPipeline() {
+		Thread server = new Thread(() -> {
+			try {
+				GrpcServer.main(null);
+			} catch (Exception e1) {
+				;
+			}
+		});
+		server.start();
+		//wait for server to start for half a second
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e1) {
+			;
+		}
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw, true);
+		try {
+			GrpcClient.runClient(new Scanner("1\n" + "F\n" + "B\n"
+					+ "test/productsums/resources/E2Einput.txt\n"
+					+ "test/productsums/resources/E2Eoutput.txt\n" + ";\n" + "2\n"), pw);
+		} catch (Exception e) {
+			pw.println(e.getMessage());
+		}
+		Assertions.assertTrue(sw.toString().contains("3:6;4:8;7:12;"));
+		server.interrupt();
+	}
+
+	public <K> void assertEqualsGeneric(K k1, K k2, int input) {
+		Assertions.assertEquals(k1, k2,
+				String.format("Pipeline returned wrong value for k = %d  \r\nExpected: %s\r\nActual: %s", input,
+						k1.toString(), k2.toString()));
+	}
+}
